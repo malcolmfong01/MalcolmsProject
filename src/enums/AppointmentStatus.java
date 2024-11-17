fcc8cd378803ccaa8933a3213c2dbed3bdf29a75
Carwyn Yeo Ruizhi <144221886+carwynyeo@users.noreply.github.com>
2024-11-17 14:21:28 +0800

    Add files via upload

--- src/enums/AppointmentStatus.java
+++ src/enums/AppointmentStatus.java
@@ -1,10 +1,18 @@
 package enums;
 
 import model.RecordStatusType;
-
+/**
+ * Enum representing the status of an appointment.
+ * It has multiple possible values: CONFIRMED, CANCELED, COMPLETED, AVAILABLE, and PENDING.
+ */
 public enum AppointmentStatus {
     CONFIRMED, CANCELED, COMPLETED, AVAILABLE, PENDING;
-
+    /**
+     * Overrides the default toString() method to provide a string representation of
+     * the AppointmentStatus enum.
+     *
+     * @return A string representing the status (e.g., "PENDING", "CONFIRMED", "CANCELED", etc.).
+     */
     @Override
     public String toString() {
         switch (this) {
@@ -22,7 +30,12 @@ public enum AppointmentStatus {
                 return "Unknown";
         }
     }
-
+    /**
+     * Converts a string to the corresponding AppointmentStatus enum value.
+     *
+     * @param status The string representation of the appointment status.
+     * @return The corresponding AppointmentStatus enum value, or null if the input string does not match any status.
+     */
     public static AppointmentStatus toEnumAppointmentStatus(String status) {
         switch (status) {
             case "PENDING":
--- src/enums/PrescriptionStatus.java
+++ src/enums/PrescriptionStatus.java
@@ -1,8 +1,18 @@
 package enums;
-
+/**
+ * Represents the status of a prescription in the system.
+ * This enum defines two possible statuses for a prescription:
+ * <ul>
+ *     <li>PENDING - Prescription has been issued but not yet dispensed.</li>
+ *     <li>DISPENSED - Prescription has been dispensed to the patient.</li>
+ * </ul>
+ */
 public enum PrescriptionStatus {
 	PENDING, DISPENSED;
-	
+	 /**
+     * Returns a user-friendly string representation of the prescription status.
+     * @return a string representing the prescription status (either "Pending" or "Dispensed").
+     */
 	@Override
     public String toString() {
         switch (this) {
@@ -14,7 +24,11 @@ public enum PrescriptionStatus {
                 return "Unknown";
         }
     }
-
+	   /**
+     * Converts a string value to its corresponding enum.
+     * @param status the string representation of the status (e.g., "Pending", "Dispensed").
+     * @return the corresponding enum value, or null if the string does not match any status.
+     */
     public static PrescriptionStatus toEnumPrescriptionStatus(String status) {
         switch (status) {
         	case "Pending":
